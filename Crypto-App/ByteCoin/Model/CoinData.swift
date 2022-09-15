4de7e10b7420ed1cfc6b00e8a176582066e80a38
Irem Tektas <tektasirem13@gmail.com>
2022-09-15 22:50:28 +0300

    Add files via upload

---
+++ Crypto-App/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by iremt on 14.09.2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData:Decodable{
+    
+    let rate : Double
+}
